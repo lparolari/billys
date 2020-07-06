0626cc64243170764d00efb776739a40061a6a32
lparolari <luca.parolari23@gmail.com>
2020-07-06 17:32:03 +0200

    Add ocr dump in pipeline

--- billys/checkpoint.py
+++ billys/checkpoint.py
@@ -4,17 +4,16 @@ from datetime import datetime
 from pickle import dump, load
 import ntpath
 
-from billys.pipeline import Step
 from billys.util import get_data_home
 
 
-def save(step: Step, obj, data_home=None):
+def save(step: str, obj, data_home=None):
     """
     Save the given object as checkpoint. 
 
     Parameters
     ----------
-    step: Step, required
+    step: str, required
         The checkpoint step.
 
     data_home : optional, default: None
@@ -28,24 +27,24 @@ def save(step: Step, obj, data_home=None):
         path = os.path.join(get_data_home(), 'checkpoints')
     else:
         path = data_home
-    name = f'{now_str}_step-{int(step)}.pkl'
+    name = f'{now_str}_step-{step}.pkl'
 
     filename = os.path.join(path, name)
 
     os.makedirs(os.path.dirname(filename), exist_ok=True)
     dump(obj, open(filename, 'wb'))
 
-    return name
+    return filename
 
 
-def revert(step: Step, data_home=None):
+def revert(prev_step: str, data_home=None):
     """
     Load the last dataset checkpoint for the step `step`.
 
     Parameters
     ----------
-    step: Step, required
-        The checkpoint step.
+    prev_step: str, required
+        The step from which we have to load checkpoint.
 
     data_home : optional, default: None
         A path for checkpoints object. 
@@ -56,14 +55,6 @@ def revert(step: Step, data_home=None):
         path = os.path.join(get_data_home(), 'checkpoints')
     else:
         path = data_home
-
-    # Note that we need to load the checkpoint from the previous phase, so for current step
-    # we are interested in the previous checkpoint.
-
-    if (int(step) - 1 < 0):
-        raise AssertionError('You must load a checkpoint only for >= 1 steps.')
-
-    prev_step = Step(int(step) - 1)
 
     # search for pkl files in dir
     checkpoints = glob.glob(f'{path}/*.pkl')
@@ -76,13 +67,13 @@ def revert(step: Step, data_home=None):
     checkpoints = [checkpoint.split('_') for checkpoint in checkpoints]
     # keep only filenames with desidered step
     checkpoints = [checkpoint[0]
-                   for checkpoint in checkpoints if checkpoint[1] == f'step-{int(prev_step)}']
+                   for checkpoint in checkpoints if checkpoint[1] == f'step-{prev_step}']
     # sort in non increasing order
     checkpoints.sort(reverse=True)
     # keep the first result, i.e., the newest checkpoint
     last = checkpoints[0]
 
     # assemble the checkpoint filename
-    name = f'{last}_step-{int(prev_step)}.pkl'
+    name = f'{last}_step-{prev_step}.pkl'
 
     return load(open(f'{os.path.join(path, name)}', 'rb'))
