3ba0e54736c316b7aede17f679bb6ecd2529d480
lparolari <luca.parolari23@gmail.com>
2020-07-06 17:31:48 +0200

    Update ocr returned output

--- billys/ocr/ocr.py
+++ billys/ocr/ocr.py
@@ -16,7 +16,7 @@ def thresholding(image):
     return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
 
-def ocr_image(image):
+def ocr_data(image):
     image = get_grayscale(image)
     image = thresholding(image)
 
@@ -25,7 +25,23 @@ def ocr_image(image):
     #text = pytesseract.image_to_string(img, config=custom_config)
     # print(text)
 
-    d = pytesseract.image_to_string(
+    d = pytesseract.image_to_data(
         image, output_type=pytesseract.Output.DICT, config=custom_config)
+    # dict_keys(['level', 'page_num', 'block_num', 'par_num', 'line_num', 'word_num', 'left', 'top', 'width', 'height', 'conf', 'text'])
 
-    return d['text']
+    return d
+
+
+# def ocr_text(image):
+#     image = get_grayscale(image)
+#     image = thresholding(image)
+
+#     # Adding custom options
+#     custom_config = r'-c tessedit_char_whitelist=0123 --psm 6 -l ita'
+#     #text = pytesseract.image_to_string(img, config=custom_config)
+#     # print(text)
+
+#     d = pytesseract.image_to_string(
+#         image, output_type=pytesseract.Output.DICT, config=custom_config)
+
+#     return d['text']
