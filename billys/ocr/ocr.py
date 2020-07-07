e6ef7219e13a18b18fa5a0a9c7edb281926ee938
lparolari <luca.parolari23@gmail.com>
2020-07-07 14:12:00 +0200

    Update pytesseract usage
    Removed get_grayscale and threshold as pytesseract already do them itself
    Changed tessract configuration

--- billys/ocr/ocr.py
+++ billys/ocr/ocr.py
@@ -2,46 +2,30 @@ import cv2
 import pytesseract
 
 
-def get_grayscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-
-def remove_noise(image):
-    return cv2.medianBlur(image, 5)
-
-
-def thresholding(image):
-    # threshold the image, setting all foreground pixels to
-    # 255 and all background pixels to 0
-    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-
-
 def ocr_data(image):
-    image = get_grayscale(image)
-    image = thresholding(image)
+    """
+    Perform the ocr on the image `image` and return a dict with ocr results.
 
-    # Adding custom options
-    custom_config = r'-c tessedit_char_whitelist=0123 --psm 6 -l ita'
-    #text = pytesseract.image_to_string(img, config=custom_config)
-    # print(text)
+    Parameters
+    ----------
+    image
+        The image to ocr.
 
-    d = pytesseract.image_to_data(
+    Returns
+    -------
+    ocr: dict
+        A dict with ocr data extracted with pytesseract.
+        The dict is composed by the following columns:
+            'level', 'page_num', 'block_num', 'par_num', 'line_num', 'word_num',
+            'left', 'top', 'width', 'height', 'conf', 'text'
+    """
+
+    # In order to know available configurations, run
+    # `tesseract --help-extra` and look for the section OCR options.
+    # Our configuration specifies
+    # * the ocr engine mode (--oem) as 1, that is 'Neural nets LSTM engine only.'
+    # * the page segmentation mode (--psm) as 3, that is 'Fully automatic page segmentation, but no OSD. (Default)'
+    custom_config = r'--oem 1 --psm 3 -l ita'
+
+    return pytesseract.image_to_data(
         image, output_type=pytesseract.Output.DICT, config=custom_config)
-    # dict_keys(['level', 'page_num', 'block_num', 'par_num', 'line_num', 'word_num', 'left', 'top', 'width', 'height', 'conf', 'text'])
-
-    return d
-
-
-# def ocr_text(image):
-#     image = get_grayscale(image)
-#     image = thresholding(image)
-
-#     # Adding custom options
-#     custom_config = r'-c tessedit_char_whitelist=0123 --psm 6 -l ita'
-#     #text = pytesseract.image_to_string(img, config=custom_config)
-#     # print(text)
-
-#     d = pytesseract.image_to_string(
-#         image, output_type=pytesseract.Output.DICT, config=custom_config)
-
-#     return d['text']
