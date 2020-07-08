3e11cc8154023a9efb91ffc7095028e3a5e017ea
lparolari <luca.parolari23@gmail.com>
2020-07-08 10:00:02 +0200

    Update dataset management with relaxed assertions

---
+++ billys/constant.py
@@ -0,0 +1,2 @@
+"""A list of supported images file format"""
+BILLYS_SUPPORTED_IMAGES_FILE_LIST = ['jpg', 'jpeg', 'png', 'pdf']/
--- billys/dataset.py
+++ billys/dataset.py
@@ -4,6 +4,7 @@ Manage dataset interaction.
 
 import os
 import pathlib
+import logging
 
 import cv2
 import pandas as pd
@@ -12,6 +13,7 @@ import sklearn.datasets
 from pdf2image import convert_from_path
 
 from billys.util import get_data_home, get_data_tmp
+from billys.constant import BILLYS_SUPPORTED_IMAGES_FILES
 
 
 def fetch_billys(data_home=None,
@@ -91,38 +93,34 @@ def make_dataframe(dataset: sklearn.utils.Bunch, force_good=False):
         A new dataframe with the following columns
          * 'filename', the path to image or pdf file,
          * 'target', the encoded values for target names,
-         * 'data', the image data read with `cv2.imread(path)`,
          * 'grayscale', whether the image is in greyscale,
-         * 'smart_doc', whether image is from smartdoc dataset,
-         * 'good', whether the image is good, i.e., it does not require dewarping,
-         * 'is_pdf', whether the image file is in pdf format.
+         * 'is_good', whether the image is good, i.e., it does not require dewarping,
+         * 'is_pdf', whether the image file is in pdf format,
+         * 'is_valid', whether the file can be used in pipeline or not.
     """
-    df = pd.DataFrame(columns=['filename', 'target',
-                               'data', 'grayscale', 'smart_doc'])
+    df = pd.DataFrame(columns=['filename', 'target', 'grayscale', 'good', 'is_pdf', 'is_valid'])
 
     df['filename'] = dataset.filenames
     df['target'] = dataset.target
-    df['data'] = [read_file(filename) for filename in dataset.filenames]
     df['grayscale'] = False
-    df['smart_doc'] = False
-    df['good'] = [filename.endswith(
-        '.pdf') or force_good for filename in dataset.filenames]
-    df['is_pdf'] = [filename.endswith('.pdf')
-                    for filename in dataset.filenames]
+    df['is_good'] = [is_good(filename, force_good) for filename in dataset.filenames]
+    df['is_pdf'] = [is_pdf(filename) for filename in dataset.filenames]
+    df['is_valid'] = [is_valid(filename) for filename in dataset.filenames]
 
     return df
 
 
-def read_file(filename: str):
+def read_file(df_row):
     """
-    Read pdf, jpg and png file data and return it.
+    Read the file data if it is supported and return it. If the file
+    is not supported, we ignore it.
 
     Parameters
     ----------
-    filename: str
-        The path to the file to read. File extension must be one of
-        'jpg', 'png' or 'pdf'. If the file if a pdf file, it is converted to an image
-        with `pdf2image` module and then its data is read with `cv2`.
+    df_row: 
+        A dataframe row containing the image metadata.
+        Required columns are
+            'filename', 'is_valid', 'is_pdf'
 
     Returns
     -------
@@ -130,22 +128,90 @@ def read_file(filename: str):
         The image data encoded with cv2 format, i.e., a list with shape
         [w, h, number of channels].
     """
-    filelower = filename.lower()
+    
+    filename = df_row['filename']
+    is_valid = df_row['is_valid']
+    is_pdf = df_row['is_pdf']
 
-    if filelower.endswith('pdf'):
-        # Convert pdf to image and the store the image data.
-        pages = convert_from_path(filename)
-        tmp_filename = os.path.join(get_data_tmp(), 'pdf.jpg')
-        os.makedirs(os.path.dirname(tmp_filename), exist_ok=True)
-        for page in pages:
-            page.save(tmp_filename, 'JPEG')
-            # As specification, we need only the first page.
-            break
-        return cv2.imread(tmp_filename)
-
-    elif filelower.endswith('jpg') or filelower.endswith('png'):
-        # Directly load the image data.
-        return cv2.imread(filename)
+    if not is_valid:
+        logging.warning(f'The file {filename} is not supported. Skipping.')
     else:
-        raise AssertionError('Supported file types are {}, you gived {}.'.format(
-            'pdf, jpg or png', pathlib.Path(filename).suffix))
+        if is_pdf
+
+            # Convert pdf to image and the store the image data.
+            pages = convert_from_path(filename)
+            tmp_filename = os.path.join(get_data_tmp(), 'pdf.jpg')
+            os.makedirs(os.path.dirname(tmp_filename), exist_ok=True)
+            for page in pages:
+                page.save(tmp_filename, 'JPEG')
+                # As specification, we need only the first page.
+                break
+            return cv2.imread(tmp_filename)
+
+        else:
+            # Directly load the image data. We do not check the 
+            # image format because we assume that it is valid.
+            return cv2.imread(filename)
+
+
+def is_good(filename: str, force: bool) -> bool:
+    """
+    Verify whether a file is good or not, i.e., it does not need
+    any shape or illumination modification. By default a file is 
+    good if it is a pdf.
+
+    Parameters
+    ----------
+    filename
+        The file name.
+    force
+        Wether to force the goodness of a file.
+
+    Returns
+    -------
+    good
+        True whether the file is a pdf or its goodnes is forced,
+        False otherwise.
+    """
+    return filename.endswith('.pdf') or force
+
+
+def is_pdf(filename: str) -> bool:
+    """
+    Verify whether a file is a pdf or not.
+
+    Parameters
+    ----------
+    filename
+        The file name.
+
+    Returns
+    -------
+    is_pdf
+        True whether the file is a pdf, False otherwise.
+    """
+    return filename.endswith('.pdf')
+
+
+def is_valid(filename: str) -> bool:
+    """
+    Verify whether a file is good or not for the pipeline.
+
+    Parameters
+    ----------
+    filename
+        The file name.
+
+    Returns
+    -------
+    is_valid
+        True whether the file is a good file for the pipeline, i.e.,
+        it is a supported file, False otherwise.
+    """
+    splitted = os.path.splitext(filename)
+
+    if len(splitted) < 2:
+        return False
+    
+    ext = splitted[1]
+    return ext in BILLYS_SUPPORTED_IMAGES_FILE_LIST
